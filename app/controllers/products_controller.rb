3d595205866ed9325365fc679557d52a2f2a1f68
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-13 12:11:59 -0500

    adds 3 individual product routes and methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,17 @@ def all_products
   product = Product.all
   render json: product.as_json
 end
+def on_cloudflow
+  cloudflow = Product.find_by(name: "on Cloudflow")
+  render json: cloudflow.as_json
+end
+def on_cloud
+  cloud = Product.find_by(name: "on Cloud")
+  render json: cloud.as_json
+end
+def on_cloudsurfer_6
+  cloudsurfer_6 = Product.find_by(name: "on Cloudsurfer 6")
+  render json: cloudsurfer_6.as_json
+end
+
 end
