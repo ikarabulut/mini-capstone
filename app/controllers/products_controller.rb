69c0c2c3d6d58e556150825ce1ad633e5a0cc87e
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-14 13:54:05 -0500

    updates initial data names and adds search query route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,26 @@
 class ProductsController < ApplicationController
+  
 def all_products
   product = Product.all
   render json: product.as_json
 end
 def on_cloudflow
-  cloudflow = Product.find_by(name: "on Cloudflow")
+  cloudflow = Product.find_by(name: "on_Cloudflow")
   render json: cloudflow.as_json
 end
 def on_cloud
-  cloud = Product.find_by(name: "on Cloud")
+  cloud = Product.find_by(name: "on_Cloud")
   render json: cloud.as_json
 end
-def on_cloudsurfer_6
-  cloudsurfer_6 = Product.find_by(name: "on Cloudsurfer 6")
+def on_cloudsurfer6
+  cloudsurfer6 = Product.find_by(name: "on_Cloudsurfer6")
   render json: cloudsurfer_6.as_json
+end
+
+def product_search
+  product_name = params["product_name"]
+  product = Product.find_by(name: product_name)
+  render json: product.as_json
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/all_products" => "products#all_products"
   get "/on_cloudflow" => "products#on_cloudflow"
   get "/on_cloud" => "products#on_cloud"
-  get "/on_cloudsurfer_6" => "products#on_cloudsurfer_6"
+  get "/on_cloudsurfer6" => "products#on_cloudsurfer6"
+  get "/product/:product_name" => "products#product_search"
 
 end
