8086d80c1ad899111adf661da918974e3a22df9f
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-15 14:17:45 -0500

    adds delete request and destroy action and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,6 +32,13 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+
+  def destroy
+    product_name = params["id"]
+    product = Product.find_by(id: product_name)
+    product.destroy
+    render json: {message: "Product succesfully deleted"}
+  end
 end
 
 
