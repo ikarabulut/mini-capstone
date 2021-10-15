e5de2f4e4278efb6165e0c0f921e0939912aef7e
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-15 11:55:32 -0500

    replaces routes with single index route and show route and controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,25 +1,13 @@
 class ProductsController < ApplicationController
   
-def all_products
+def index
   product = Product.all
   render json: product.as_json
 end
-def on_cloudflow
-  cloudflow = Product.find_by(name: "on_Cloudflow")
-  render json: cloudflow.as_json
-end
-def on_cloud
-  cloud = Product.find_by(name: "on_Cloud")
-  render json: cloud.as_json
-end
-def on_cloudsurfer6
-  cloudsurfer6 = Product.find_by(name: "on_Cloudsurfer6")
-  render json: cloudsurfer_6.as_json
-end
 
-def product_search
-  product_name = params["product_name"]
-  product = Product.find_by(name: product_name)
+def show
+  product_name = params["id"]
+  product = Product.find_by(id: product_name)
   render json: product.as_json
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/all_products" => "products#all_products"
-  get "/on_cloudflow" => "products#on_cloudflow"
-  get "/on_cloud" => "products#on_cloud"
-  get "/on_cloudsurfer6" => "products#on_cloudsurfer6"
-  get "/product/:product_name" => "products#product_search"
+  get "/products" => "products#index"
+  get "products/:id" => "products#show"
 
 end
