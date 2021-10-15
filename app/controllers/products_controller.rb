9c1f831bca40c49d51d5359bbb3aaa9301c2b889
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-15 12:56:46 -0500

    adds post request with a create action and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,26 @@
 class ProductsController < ApplicationController
   
-def index
-  product = Product.all
-  render json: product.as_json
+  def index
+    product = Product.all
+    render json: product.as_json
+  end
+
+  def show
+    product_name = params["id"]
+    product = Product.find_by(id: product_name)
+    render json: product.as_json
+  end
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    product.save
+    render json: product.as_json
+  end 
 end
 
-def show
-  product_name = params["id"]
-  product = Product.find_by(id: product_name)
-  render json: product.as_json
-end
 
-end
