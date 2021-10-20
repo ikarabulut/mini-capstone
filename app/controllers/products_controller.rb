a161f6a84f01c9b49f4e0cb9b80002a7627d35aa
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-20 13:12:48 -0500

    adds validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,11 @@ class ProductsController < ApplicationController
       image_url: params["image_url"],
       description: params["description"],
     )
-    product.save
-    render json: product
+    if product.save
+      render json: product
+    else 
+      render json: {errors: product.errors.full_messages}, status: 422
+    end 
   end 
 
   def update
@@ -29,8 +32,11 @@ class ProductsController < ApplicationController
     product.price = params["price"] || product.price
     product.image_url = params["image_url"] || product.image_url
     product.description = params["description"] || product.description
-    product.save
-    render json: product
+    if product.save
+      render json: product
+    else 
+      render json: {errors: product.errors.full_messages}, status: 406
+    end 
   end
 
   def destroy
