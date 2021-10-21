d2f333e5a023e1e6af095197c7cbc3d0cf7a0e3d
Ismail Karabulut <i.karabulut93@gmail.com>
2021-10-21 15:03:13 -0500

    removes image_url from products create and update method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,6 @@ class ProductsController < ApplicationController
     product = Product.new(
       name: params["name"],
       price: params["price"],
-      image_url: params["image_url"],
       description: params["description"],
     )
     if product.save
@@ -30,7 +29,6 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: product_name)
     product.name = params["name"] || product.name
     product.price = params["price"] || product.price
-    product.image_url = params["image_url"] || product.image_url
     product.description = params["description"] || product.description
     if product.save
       render json: product
